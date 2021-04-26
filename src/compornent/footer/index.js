7c88be1f7ff4cf3ab596f52ed2c34a3c34152db6
lequyet2001 <lequyet840500@gmail.com>
2021-04-26 10:04:34 +0700

    update

--- src/compornent/footer/index.js
+++ src/compornent/footer/index.js
@@ -4,7 +4,7 @@ export default class Footer extends Component {
     render() {
         return (
             <footer className="">
-                <div class="footer__body">
+                <div className="footer__body">
                     <p>@copyright to quyet <a href="">2021</a></p>
                 </div>
             </footer>
